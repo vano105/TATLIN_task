d96812323ed83c14ea8897a6bd973dd84cff50a8
vano105 <ivankhromov05@mail.ru>
2025-05-04 10:30:43 +0400

    Add config file and config parser

---
+++ src/config/config.cpp
@@ -0,0 +1,32 @@
+#include "config.hpp"
+#include <fstream>
+#include <sstream>
+
+config load_config(const std::string &file_name) {
+  config config;
+  std::ifstream file(file_name);
+  std::string line;
+
+  while (std::getline(file, line)) {
+    std::istringstream iss(line);
+    std::string key;
+
+    if (std::getline(iss, key, '=')) {
+      std::string value_str;
+      if (std::getline(iss, value_str)) {
+        if (key == "memory_limit")
+          config.memory_limit = std::stoul(value_str);
+        else if (key == "read_delay")
+          config.read_delay = std::stoul(value_str);
+        else if (key == "write_delay")
+          config.write_delay = std::stoul(value_str);
+        else if (key == "shift_delay")
+          config.shift_delay = std::stoul(value_str);
+        else if (key == "rewind_delay_per_position")
+          config.rewind_delay_per_position = std::stod(value_str);
+      }
+    }
+  }
+
+  return config;
+}
---
+++ src/config/config.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+
+struct config {
+public:
+  size_t memory_limit = 1024 * 1024;
+  size_t read_delay = 0;
+  size_t write_delay = 0;
+  size_t shift_delay = 0;
+  double rewind_delay_per_position = 0.0;
+
+  config load_config(const std::string &file_name);
+};
